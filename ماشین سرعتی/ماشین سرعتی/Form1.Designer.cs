8ff9183345491555c2abe6b8862955a5725db6e7
mahyar <mahyarmogaddam4050@gmail.com>
2024-12-04 08:20:28 +0330

    Add files via upload

---
+++ ماشین سرعتی/ماشین سرعتی/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace ماشین_سرعتی
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picBarrier = new System.Windows.Forms.PictureBox();
+            this.picCar = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picBarrier)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBarrier
+            // 
+            this.picBarrier.Image = global::ماشین_سرعتی.Properties.Resources.icons8_obstacle_50;
+            this.picBarrier.Location = new System.Drawing.Point(1426, 180);
+            this.picBarrier.Name = "picBarrier";
+            this.picBarrier.Size = new System.Drawing.Size(100, 83);
+            this.picBarrier.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBarrier.TabIndex = 1;
+            this.picBarrier.TabStop = false;
+            // 
+            // picCar
+            // 
+            this.picCar.Image = global::ماشین_سرعتی.Properties.Resources.icons8_car_50;
+            this.picCar.Location = new System.Drawing.Point(12, 180);
+            this.picCar.Name = "picCar";
+            this.picCar.Size = new System.Drawing.Size(100, 83);
+            this.picCar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCar.TabIndex = 0;
+            this.picCar.TabStop = false;
+            this.picCar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.picCar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1541, 274);
+            this.Controls.Add(this.picBarrier);
+            this.Controls.Add(this.picCar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBarrier)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picCar;
+        private System.Windows.Forms.PictureBox picBarrier;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ماشین سرعتی/ماشین سرعتی/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ماشین_سرعتی
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            picCar.Left += 5;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            timer1.Enabled = false;
+            if ((picCar.Right + 10 > picBarrier.Left) &&
+                (picCar.Right < picBarrier.Left))
+                MessageBox.Show("you win");
+            else
+                MessageBox.Show("you lose");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
